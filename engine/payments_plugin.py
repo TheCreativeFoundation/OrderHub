8a07d657d4079c7a87d3df475e9dffa51af203b2
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-07-02 10:09:53 -0700

    Finished the payment plugins charge_customer method

--- engine/payments_plugin.py
+++ engine/payments_plugin.py
@@ -3,13 +3,33 @@ import stripe
 
 class PaymentsPlugin(object):
     def __init__(self, stripe_api_key: str):
-        stripe.api_key = stripe_api_key
+        self.stripe_client = stripe.api_key
         self.stripe_api_key = stripe_api_key
 
-    def create_payment(self, payment_info: dict, order_info: dict):
+    def charge_customer(self, cost: int, description: str, stripe_token) -> int:
         status_code = 505
-        try:
-            pass
-        except Exception as e:
-            print(e)
+        currency = "usd"
+        if stripe_token != None:
+            try:
+                charge = stripe.Charge.create(
+                    amount=cost,
+                    currency=currency,
+                    description=description,
+                    source=stripe_token
+                )
+                if charge:
+                    status_code = 202
+            except stripe.error.CardError as e:
+                print(e)
+                status_code = 415 # CARD DECLINED ERROR
+            except stripe.error.APIConnectionError as e:
+                print(e)
+                status_code = 416 # NETWORK ERROR
+            except stripe.error.AuthenticationError as e:
+                print(e)
+                status_code = 417 # AUTH ERROR
+            except Exception as e:
+                print(e)
+        else:
+            status_code = 408 # MISSING STRIPE TOKEN FOR PAYMENT
         return status_code
