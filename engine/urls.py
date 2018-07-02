63e0d47b5af2363951a07540c3927a4ef3a7c272
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-07-02 10:11:17 -0700

    Added all urls to engine for now

--- engine/urls.py
+++ engine/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r"", views.index)
+    path('', views.index),
+    path('order/complete', views.CompleteOrderView.as_view()),
+    path('order/send', views.SendOrderView.as_view()),
+    path('/order/send/success', views.SendOrderSuccessView.as_view()),
+    path('/order/send/failed', views.SendOrderFailedView.as_view()),
+    
 ]
