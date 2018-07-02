97a2f7b669c50343f901bc57c4282d5fbf2f18f6
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-07-02 10:08:41 -0700

    Filled out gateway view without caching

--- gateway/views.py
+++ gateway/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from __future__ import print_function
+from django.shortcuts import render, redirect
 from django.views import View
+from django.http import HttpResponse
+import json
+import requests
 
 class GatewayView(View):
-    template_name = "gateway.html"
-
-    def get(self, request):
-        pass
     
-    def post(self, request):
-        pass
+    def get(self, request) -> HttpResponse:
+        body: dict = json.loads(request.body.decode("utf-8"))
+        template_data: dict = {"cost":"$"+str(body["cost"])}
+        return render(request, "gateway.html, template_data)
+
